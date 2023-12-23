39e1e54c223e9bf3de91757a4aef7c13a9c8eb59
chitradevig23 <chitradevig23@gmail.com>
2023-12-23 13:53:06 +0530

    update project

---
+++ src/components/navbar.js
@@ -0,0 +1,15 @@
+//creating navbar component
+function Navbar(){
+    return(
+        <nav>
+            <h1>Perfumy</h1>
+            <ul>
+                <li>Home |</li>
+                <li>Product |</li>
+                <li>Contact |</li>
+                <li>About us |</li>
+            </ul>
+        </nav>
+    )
+}
+export default Navbar
---
+++ src/components/search.js
@@ -0,0 +1,9 @@
+//search component
+function Search(){
+    return(
+        <div className="searchbar">
+            <input placeholder="Search"></input>
+        </div>
+    )
+}
+export default Search
